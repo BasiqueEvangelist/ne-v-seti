13270e912a991d9e1c4e32cfeab56fc518b26d9e
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-04-04 19:02:57 +0300

    Import UUID

--- src/main/java/me/basiqueevangelist/nevseti/OfflineAdvancementUtils.java
+++ src/main/java/me/basiqueevangelist/nevseti/OfflineAdvancementUtils.java
@@ -7,6 +7,7 @@ import net.minecraft.util.Identifier;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public final class OfflineAdvancementUtils {
     private OfflineAdvancementUtils() {
